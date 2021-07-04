20b421239f674b3787928493191aded79dc5b3aa
FaigBagirov <bagirov3105@gmail.com>
2021-07-04 23:29:52 +0400

    first commit

---
+++ en/js/main.js
@@ -0,0 +1,74 @@
+
+
+
+
+//COURSE HEADER ITEMS CHANGE
+let courseUl=document.querySelector("#courseh-ul");
+let courseli=document.querySelectorAll(".courseh-link");
+courseli.forEach(el=>
+{
+	el.addEventListener("click",function(e)
+	{
+		courseUl.querySelector(".courseh-active").classList.remove("courseh-active");
+		el.classList.add("courseh-active");	
+	
+ 		e.preventDefault();
+	});
+ 
+});
+
+
+
+
+
+var conInp=document.getElementsByClassName("ConInp");
+var spanInp=document.getElementsByClassName("ConSpan");
+for (let i=0;i<conInp.length ;i++){
+  conInp[i].addEventListener("focus", function ChangeSpan()
+  	{
+  		spanInp[i].classList.add("focusInput");
+  		spanInp[i].setAttribute("style","color:#ff568c !important");
+  	});
+  conInp[i].addEventListener("focusout",function NormalSpan(e)
+  	{
+  		if(e.target.value=="")
+{
+spanInp[i].classList.remove("focusInput");
+spanInp[i].setAttribute("style","color:#333 !important")
+}
+else
+{
+	spanInp[i].classList.add("focusInput");
+}
+  	});
+}
+
+  /*-----------MODAL--------------*/
+
+  (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+  }, false);
+  });
+}, false);
+  })()
+// const currentLocation=location.href;
+// console.log(currentLocation);
+// const courseItem=document.querySelectorAll(".courseh-link");
+// for (var i = 0; i < courseItem.length; i++) {
+// 	if(courseItem[i].href===currentLocation)
+// 	{
+// 		courseItem[i].className="courseh-active";
+// 	}
+	
+// }
